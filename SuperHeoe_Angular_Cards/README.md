3ebfc690f5809f46e512fcbf29b8215912360002
Juan Carlos Valderrábano Hernández <jcvalderrabanoh@outlook.com>
2019-10-29 19:32:48 -0600

    “feature”

---
+++ SuperHeoe_Angular_Cards/README.md
@@ -0,0 +1,7 @@
+# Bienvenido 
+
+Software básico de demostración de angular 
+
+# Descripción
+
+Pequeña Plantilla de Busqueda de Super Heroes en Angular
---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
+
+
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes : Heroe[] = [];
+  constructor( private _heroeService: HeroesService ) {
+    console.log("Constructor");
+  }
+
+  ngOnInit() {
+    this.heroes = this._heroeService.getHeroes();
+    console.log(this.heroes);
+  }
+
+
+    verHeroe( idx:number ){
+        console.log(idx);
+    }
+}
